3fd5121351012a7f7848396d3db7fbb2329a1bbc
hazemyasser123 <153863853+hazemyasser123@users.noreply.github.com>
2024-04-08 21:26:53 +0200

    Update Array_Of_Pointers.h

--- Project1/Array_Of_Pointers.h
+++ Project1/Array_Of_Pointers.h
@@ -12,11 +12,11 @@ class Array_Of_Pointers
 private:
 	T *items[size];
 	/******************************************************************************************************************/
-	//		------------        /\    /\        | )    :   when using this class you declare it like this " Array_Of_Pointers<int> "                                                                        
-	//            |			   /  \  /  \       |   )  :   and the class will make it an array of pointers from this data type in other words you can only                                                                       
+	//	------------        /\    /\        | )    :   when using this class you declare it like this " Array_Of_Pointers<int> "                                                                        
+	//            |		   /  \  /  \       |   )  :   and the class will make it an array of pointers from this data type in other words you can only                                                                       
 	//            |           /    \/    \      |   )  :   insert in this form " int* " (Replace int with whatever data type you are using)                                                                                     
-	//            |          /            \     |  )   :   (IF YOU DON'T UNDERSTAND WHAT I WROTE TELL ZUMA BEEH)                                                                       
-	//            |         /              \    | )    :                                                                          
+	//            |          /            \     |  )   :   (IF YOU DON'T UNDERSTAND WHAT I WROTE CONTACT ME)                                                                        
+	//            |         /              \    | )    :    ZUMA BEEH                                                                      
 	//      ------------   /                \   |    
 	//                                                                                         
 	/******************************************************************************************************************/
