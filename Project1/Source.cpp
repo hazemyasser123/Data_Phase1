4f7af3aa8370170281bc7b9fc81c563127e1197a
hazemyasser123 <hazemyasser6@gmail.com>
2024-04-16 22:50:02 +0200

    nothing

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 int main()
 {
-	cout << "============== Creators ==============" << endl ;
-	cout << "Zuma beeh" << endl;
-	cout << "Doc/Omar" << endl;
-	cout << "M. Tamer" << endl;
-	cout << "Haitham" << endl;
-
 	Game g2;
 	for (int i = 0; i < 50; i++)
 	{
