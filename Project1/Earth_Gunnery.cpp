d1eba2bfb1f1ccf6ca82be2176cf7edf9ad911d6
hazemyasser123 <hazemyasser6@gmail.com>
2024-04-25 17:40:46 +0200

    Finished the EG attack function

--- Project1/Earth_Gunnery.cpp
+++ Project1/Earth_Gunnery.cpp
@@ -1,5 +1,5 @@
 #include "Earth_Gunnery.h"
-
+#include "Game.h"
 
 Earth_Gunnery::Earth_Gunnery() :Unit()
 {
@@ -11,7 +11,112 @@ Earth_Gunnery::Earth_Gunnery(int id, int tj, int h, int p, int ac, Game* Gp) :Un
 	SetType("Earth_Gunnery");
 }
 
-void Earth_Gunnery::Attack()
+
+void Earth_Gunnery::Attack(UnitQueue& Temp_List)
 {
-	// TO DO: It's implementation will be done in phase 2
+	int NumberUnitsToBeAttacked;
+	Unit* UnitToBeAttacked;
+	Unit* UnitToBeAttacked2; // This is made for the case of attacking two drones 
+	if (GetAttackCap() % 2 == 0)
+	{
+		NumberUnitsToBeAttacked = GetAttackCap() / 2;
+		for (int i = 0; i < NumberUnitsToBeAttacked; i++)
+		{
+			UnitToBeAttacked = GetGamePtr()->GetAlienArmy()->PickAM();
+			if (UnitToBeAttacked != nullptr)
+			{
+				UnitToBeAttacked->SetHealth(UnitToBeAttacked->GetHealth() - ((GetPower() * GetHealth() / 100) / ((UnitToBeAttacked->GetHealth()) ^ (1 / 2))));
+				UnitToBeAttacked->BeAttacked(GetPower(), GetGamePtr()->getCurrentTime());
+				if (UnitToBeAttacked->IsAlive() == true)
+				{
+					Temp_List.enqueue(UnitToBeAttacked);
+				}
+				else
+				{
+					GetGamePtr()->InsertInKilled_List(UnitToBeAttacked);
+				}
+			}
+		}
+		for (int i = 0; i < NumberUnitsToBeAttacked; i++)
+		{
+			GetGamePtr()->GetAlienArmy()->pickDrones(UnitToBeAttacked, UnitToBeAttacked2);
+			if (UnitToBeAttacked != nullptr)
+			{
+				UnitToBeAttacked->SetHealth(UnitToBeAttacked->GetHealth() - ((GetPower() * GetHealth() / 100) / ((UnitToBeAttacked->GetHealth()) ^ (1 / 2))));
+				UnitToBeAttacked->BeAttacked(GetPower(), GetGamePtr()->getCurrentTime());
+				if (UnitToBeAttacked->IsAlive() == true)
+				{
+					Temp_List.enqueue(UnitToBeAttacked);
+				}
+				else
+				{
+					GetGamePtr()->InsertInKilled_List(UnitToBeAttacked);
+				}
+			}
+			if (UnitToBeAttacked2 != nullptr)
+			{
+				UnitToBeAttacked2->SetHealth(UnitToBeAttacked2->GetHealth() - ((GetPower() * GetHealth() / 100) / ((UnitToBeAttacked2->GetHealth()) ^ (1 / 2))));
+				UnitToBeAttacked2->BeAttacked(GetPower(), GetGamePtr()->getCurrentTime());
+				if (UnitToBeAttacked2->IsAlive() == true)
+				{
+					Temp_List.enqueue(UnitToBeAttacked2);
+				}
+				else
+				{
+					GetGamePtr()->InsertInKilled_List(UnitToBeAttacked2);
+				}
+			}
+		}
+	}
+	else
+	{
+		NumberUnitsToBeAttacked = GetAttackCap() / 2;
+		for (int i = 0; i < NumberUnitsToBeAttacked + 1; i++)
+		{
+			UnitToBeAttacked = GetGamePtr()->GetAlienArmy()->PickAM();
+			if (UnitToBeAttacked != nullptr)
+			{
+				UnitToBeAttacked->SetHealth(UnitToBeAttacked->GetHealth() - ((GetPower() * GetHealth() / 100) / ((UnitToBeAttacked->GetHealth()) ^ (1 / 2))));
+				UnitToBeAttacked->BeAttacked(GetPower(), GetGamePtr()->getCurrentTime());
+				if (UnitToBeAttacked->IsAlive() == true)
+				{
+					Temp_List.enqueue(UnitToBeAttacked);
+				}
+				else
+				{
+					GetGamePtr()->InsertInKilled_List(UnitToBeAttacked);
+				}
+			}
+		}
+		for (int i = 0; i < NumberUnitsToBeAttacked; i++)
+		{
+			GetGamePtr()->GetAlienArmy()->pickDrones(UnitToBeAttacked, UnitToBeAttacked2);
+			if (UnitToBeAttacked != nullptr)
+			{
+				UnitToBeAttacked->SetHealth(UnitToBeAttacked->GetHealth() - ((GetPower() * GetHealth() / 100) / ((UnitToBeAttacked->GetHealth()) ^ (1 / 2))));
+				UnitToBeAttacked->BeAttacked(GetPower(), GetGamePtr()->getCurrentTime());
+				if (UnitToBeAttacked->IsAlive() == true)
+				{
+					Temp_List.enqueue(UnitToBeAttacked);
+				}
+				else
+				{
+					GetGamePtr()->InsertInKilled_List(UnitToBeAttacked);
+				}
+			}
+			if (UnitToBeAttacked2 != nullptr)
+			{
+				UnitToBeAttacked2->SetHealth(UnitToBeAttacked2->GetHealth() - ((GetPower() * GetHealth() / 100) / ((UnitToBeAttacked2->GetHealth()) ^ (1 / 2))));
+				UnitToBeAttacked2->BeAttacked(GetPower(), GetGamePtr()->getCurrentTime());
+				if (UnitToBeAttacked2->IsAlive() == true)
+				{
+					Temp_List.enqueue(UnitToBeAttacked2);
+				}
+				else
+				{
+					GetGamePtr()->InsertInKilled_List(UnitToBeAttacked2);
+				}
+			}
+		}
+	}
 }
--- Project1/Earth_Gunnery.h
+++ Project1/Earth_Gunnery.h
@@ -6,6 +6,6 @@ class Earth_Gunnery : public Unit  // IMPORTANT: It's DS is Priority Queue
 public:
 	Earth_Gunnery();
 	Earth_Gunnery(int id, int tj, int h, int p, int ac, Game* Gp);
-	void Attack();
+	void Attack(UnitQueue& Temp_List);
 };
 
